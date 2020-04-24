523725c7c1636a96cbff7a83166ace72ccf6bd8a
Rodrigo Mariano <rodmariano13@gmail.com>
2020-04-24 12:47:04 -0300

    Function to create the bar chart was refactored; function to extract the df based on the selected date was created

--- catalog_dash/components.py
+++ catalog_dash/components.py
@@ -16,19 +16,8 @@ pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
 
-def get_figure_of_graph_bubble_map_amount_of_scenes(df, xaxis_range=[], title=None, animation_frame=None,
-                                                    is_scatter_geo=True, sort_ascending=True,
-                                                    sort_by=['year_month', 'dataset']):
-    logging.info('get_figure_of_graph_bubble_map_amount_of_scenes()\n')
-
-    figure_height = 800
-
-    df_copy = df.copy()
-
-    logging.info('get_figure_of_graph_bubble_map_amount_of_scenes() - df_copy.head(): \n%s\n', df_copy.head())
-    logging.info('get_figure_of_graph_bubble_map_amount_of_scenes() - xaxis_range: %s\n', xaxis_range)
-
-    logical_date_range = None
+def get_logical_date_range(df, xaxis_range=None):
+    logging.info('get_logical_date_range()')
 
     # if there are values, then get a boolean df according to the selected date range
     if xaxis_range:
@@ -36,13 +25,85 @@ def get_figure_of_graph_bubble_map_amount_of_scenes(df, xaxis_range=[], title=No
         start_date = xaxis_range[0][:-3]
         end_date = xaxis_range[1][:-3]
 
-        logging.info('get_figure_of_graph_bubble_map_amount_of_scenes() - start_date: %s', start_date)
-        logging.info('get_figure_of_graph_bubble_map_amount_of_scenes() - end_date: %s\n', end_date)
+        logging.info('get_logical_date_range() - start_date: %s', start_date)
+        logging.info('get_logical_date_range() - end_date: %s\n', end_date)
 
         # extract a boolean df from the original one by the selected date range
-        logical_date_range = ((df_copy['year_month'] >= start_date) & (df_copy['year_month'] <= end_date))
+        return ((df['year_month'] >= start_date) & (df['year_month'] <= end_date))
     else:
         raise CatalogDashException('Invalid `xaxis_range`, it is empty!')
+
+
+def get_figure_of_graph_bar_ploy_amount_of_scenes(df, xaxis_range=[], title=None, animation_frame=None,
+                                                  is_scatter_geo=True, sort_ascending=True,
+                                                  sort_by=['year_month', 'dataset']):
+    logging.info('get_figure_of_graph_bar_ploy_amount_of_scenes()')
+
+    figure_height = 800
+    df_copy = df.copy()
+
+    logging.info('get_figure_of_graph_bar_ploy_amount_of_scenes() - df_copy.head(): \n%s\n', df_copy.head())
+    logging.info('get_figure_of_graph_bar_ploy_amount_of_scenes() - xaxis_range: %s\n', xaxis_range)
+
+    logical_date_range = get_logical_date_range(df_copy, xaxis_range)
+
+    # I'm goint to build the `data` parameter of `Figure`
+    data = []
+
+    # I would like to build each `bar` based on each dataset
+    for dataset in df_copy['dataset'].unique():
+        sub_df = df_copy[(df_copy['dataset'] == dataset) & logical_date_range]
+
+        hovertext = 'Amount of Scenes: ' + sub_df['amount'].map(str) + '<br>' + \
+                    'Period: ' + sub_df['year_month'].map(str) + '<br>' + \
+                    'Dataset: ' + sub_df['dataset'].map(str)
+
+        data.append(
+            Bar(
+                {
+                    'x': sub_df['year_month'],
+                    'y': sub_df['amount'],
+                    'name': dataset,
+                    'text': sub_df['amount'],  # text inside the bar
+                    'textposition': 'auto',
+                    'hovertext': hovertext,
+                }
+            )
+        )
+
+    fig = Figure(
+        {
+            'data': data,
+            'layout': {
+                'title': title,
+                'xaxis': {'title': 'Period'},
+                'yaxis': {'title': 'Amount of scenes'},
+                'plot_bgcolor': colors['background'],
+                'paper_bgcolor': colors['background'],
+                'font': {
+                    'color': colors['text']
+                }
+            }
+        }
+    )
+
+    fig.update_layout(barmode='group', height=figure_height, xaxis_tickangle=-45)
+
+    return fig
+
+
+def get_figure_of_graph_bubble_map_amount_of_scenes(df, xaxis_range=[], title=None, animation_frame=None,
+                                                    is_scatter_geo=True, sort_ascending=True,
+                                                    sort_by=['year_month', 'dataset']):
+    logging.info('get_figure_of_graph_bubble_map_amount_of_scenes()\n')
+
+    figure_height = 800
+    df_copy = df.copy()
+
+    logging.info('get_figure_of_graph_bubble_map_amount_of_scenes() - df_copy.head(): \n%s\n', df_copy.head())
+    logging.info('get_figure_of_graph_bubble_map_amount_of_scenes() - xaxis_range: %s\n', xaxis_range)
+
+    logical_date_range = get_logical_date_range(df_copy, xaxis_range)
 
     # get a sub set from the df according to the selected date range
     df_copy = df_copy[logical_date_range]
@@ -88,56 +149,5 @@ def get_figure_of_graph_bubble_map_amount_of_scenes(df, xaxis_range=[], title=No
             mapbox_style='open-street-map',
             margin={'t': 40, 'r': 5, 'b': 5, 'l': 5}
         )
-
-    return fig
-
-
-def get_figure_of_graph_bar_ploy_amount_of_scenes(df, xaxis_range=[], title=None, animation_frame=None,
-                                                  is_scatter_geo=True, sort_ascending=True,
-                                                  sort_by=['year_month', 'dataset']):
-
-    figure_height = 800
-
-    # I'm goint to build the `data` parameter of `Figure`
-    data = []
-
-    # I would like to build each `bar` based on each dataset
-    for dataset in df['dataset'].unique():
-        sub_df = df[(df['dataset'] == dataset)]
-
-        hovertext = 'Amount of Scenes: ' + sub_df['amount'].map(str) + '<br>' + \
-                    'Period: ' + sub_df['year_month'].map(str) + '<br>' + \
-                    'Dataset: ' + sub_df['dataset'].map(str)
-
-        data.append(
-            Bar(
-                {
-                    'x': sub_df['year_month'],
-                    'y': sub_df['amount'],
-                    'name': dataset,
-                    'text': sub_df['amount'],  # text inside the bar
-                    'textposition': 'auto',
-                    'hovertext': hovertext,
-                }
-            )
-        )
-
-    fig = Figure(
-        {
-            'data': data,
-            'layout': {
-                'title': 'Time series',
-                'xaxis': {'title': 'Period'},
-                'yaxis': {'title': 'Amount of scenes'},
-                'plot_bgcolor': colors['background'],
-                'paper_bgcolor': colors['background'],
-                'font': {
-                    'color': colors['text']
-                }
-            }
-        }
-    )
-
-    fig.update_layout(barmode='group', height=figure_height, xaxis_tickangle=-45)
 
     return fig
